af63b8c7edde015adc6f140d86f95d0924d9c025
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-09-22 17:29:07 -0400

    channels: fix options for rdpsnd and skel

--- channels/skel/ChannelOptions.cmake
+++ channels/skel/ChannelOptions.cmake
@@ -4,5 +4,6 @@ set(CHANNEL_SHORT_NAME "skel")
 set(CHANNEL_LONG_NAME "Skeleton Code Virtual Channel Extension")
 set(CHANNEL_SPECIFICATIONS "")
 
+string(TOUPPER "WITH_${CHANNEL_SHORT_NAME}" CHANNEL_OPTION)
 option(${CHANNEL_OPTION} "Build ${CHANNEL_SHORT_NAME}" OFF)
 
